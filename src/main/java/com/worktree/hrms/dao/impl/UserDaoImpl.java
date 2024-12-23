4afa4454bfc5fc6ff29163a4c27e28b2f8f262f2
Portabull <86007183+Portabull@users.noreply.github.com>
2024-12-23 14:20:07 +0530

    Update UserDaoImpl.java

--- src/main/java/com/worktree/hrms/dao/impl/UserDaoImpl.java
+++ src/main/java/com/worktree/hrms/dao/impl/UserDaoImpl.java
@@ -111,7 +111,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public Map<String, Object> getUsers(Integer page) {
-        int fetchSize = 100;
+        int fetchSize = 25;
         if (page == null) {
             page = 0;
         }
