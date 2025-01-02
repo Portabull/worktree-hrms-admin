dbec2833f96870e6acd7cc6dd2ccce990f6a666b
portabull <portabullmanagementsystem@gmail.com>
2025-01-02 23:20:59 +0530

    Added access origin

--- src/main/java/com/worktree/hrms/filters/SecurityFilter.java
+++ src/main/java/com/worktree/hrms/filters/SecurityFilter.java
@@ -115,6 +115,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin","https://worktree-hrms.shop");
         if (request.getRequestURI().startsWith("/api")) {
             int statusCode = HttpServletResponse.SC_UNAUTHORIZED;
             final String token = request.getHeader(HttpHeaders.AUTHORIZATION);
