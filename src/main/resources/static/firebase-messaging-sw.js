097e11fb34ad693e6a21b091f875dfa6bd2710b6
portabull <portabullmanagementsystem@gmail.com>
2025-01-16 15:47:25 +0530

    Added fcm file

---
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+importScripts('https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js');
+importScripts('https://www.gstatic.com/firebasejs/11.1.0/firebase-messaging.js');
+
+
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_ii6KwNuN8bIzeK7V8IkBzBzMFjXni8Q",
+    authDomain: "worktree-hrms.firebaseapp.com",
+    projectId: "worktree-hrms",
+    storageBucket: "worktree-hrms.firebasestorage.app",
+    messagingSenderId: "559305051722",
+    appId: "1:559305051722:web:64ac18194578e66ea260b3",
+    measurementId: "G-4QLF75ZPYT"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Background message handler
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
